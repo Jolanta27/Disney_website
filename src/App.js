b409fc8b71d5886504004cabc66f619688748b7e
Jolanta Galczynska <jolanta.galczynska@gmail.com>
2021-07-18 01:29:47 +0200

    added slider component with pictures

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+
 
 function App() {
   return (
---
+++ src/components/ImgSlider.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+function ImgSlider() {
+
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
+    }
+    return (
+        <Carousel {...settings}>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" alt="" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badag.jpg" alt=""/>
+            </Wrap>
+        </Carousel>
+    )
+}
+
+export default ImgSlider
+
+const Carousel = styled(Slider)`
+margin-top: 20px;
+
+.slick-list {
+    overflow: visible;
+}
+`
+const Wrap = styled.div`
+img {
+    border: 4px solid transparent;
+    border-radius: 4px;
+    width: 100%;
+   box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+}
+`
